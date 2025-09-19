cbe05383311b676dacb1f0e55526a226fd4da8db
Santiago Hoyos <santiagojhoyoss@gmail.com>
2025-09-18 23:08:15 -0500

    un endpoint funcional

--- src/application/auth/auth.service.ts
+++ src/application/auth/auth.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    async findAll() {
+        return `This action returns all`;
+    }
+}
--- src/interface/auth/auth.controller.ts
+++ src/interface/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { AuthService } from 'src/application/auth/auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Get()
+    findAll() {
+        return this.authService.findAll();
+    }
+}
