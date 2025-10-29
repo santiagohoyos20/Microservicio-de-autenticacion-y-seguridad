25091474dc25f5f8f2fd01dded11cb2b78852a13
Santiago Hoyos <santiagojhoyoss@gmail.com>
2025-10-29 14:51:17 -0500

    fix

--- src/application/modules/grpc.module.ts
+++ src/application/modules/grpc.module.ts
@@ -12,7 +12,7 @@ import { join } from 'path';
         options: {
           package: 'users', // debe coincidir con el "package" del .proto
           protoPath: join(process.cwd(), 'src/application/modules/users.proto'), // ruta al archivo .proto
-          url: 'localhost:50051', // dirección del microservicio users
+          url: 'users:50051', // dirección del microservicio users
         },
       },
       // Cliente para DRIVERS
@@ -22,7 +22,7 @@ import { join } from 'path';
         options: {
           package: 'drivers', // debe coincidir con el "package" del .proto
           protoPath: join(process.cwd(), 'src/application/modules/drivers.proto'),
-          url: 'localhost:50052', // dirección del microservicio drivers
+          url: 'drivers:50052', // dirección del microservicio drivers
         },
       },
     ]),
